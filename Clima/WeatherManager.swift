b359fbf168664b46e0507c6f665cb4561296a98f
zsolt13 <zsolt@bereadysoftware>
2020-02-10 15:36:37 +0100

    Initial Commit

---
+++ Clima/WeatherManager.swift
@@ -0,0 +1,68 @@
+//
+//  File.swift
+//  Clima
+//
+//  Created by zsolt on 22/01/2020.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel)
+    func didFailWithError(error: Error)
+}
+
+struct WeatherManager {
+    
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=444032a3c4ed55ecd789e1c3632b8c1d&units=metric"
+    
+    var delegate: WeatherManagerDelegate?
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        print(urlString)
+        performRequest(with: urlString)
+    }
+    
+    func performRequest(with urlString: String) {
+        //1. create an URL
+        if let url = URL(string: urlString) {
+            //2.create an URLSession
+            let session = URLSession(configuration: .default)
+            
+            //3. give the session one task
+            let task = session.dataTask(with: url) {(data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    if let weather = self.parseJson(safeData) {
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
+                }
+            }
+            //4. Start the test
+            task.resume()
+        }
+    }
+    
+    func parseJson(_ weatherData: Data) -> WeatherModel? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            return weather
+        } catch {
+            delegate?.didFailWithError(error: error)
+            return nil
+        }
+        
+        
+    }
+}
